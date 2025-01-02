e49bb6f52bff1e359e6652dabf7e6c083508d719
zolynux <152818486+zolynux@users.noreply.github.com>
2025-01-02 09:38:44 -0500

    Curso de Universidad C++ para aprender en el futuro o mejorar lógico

---
+++ cpp_01_introduccion/cpp_01_hola_mundo.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // i-input, o-output, streame
+using namespace std;
+
+// C++ -> cpp (+ plus)
+int main() {
+  // 1. Imprimir un saludo a consola
+  cout << "Hola mundo con C++";
+  return 0;
+} // Fin de main
